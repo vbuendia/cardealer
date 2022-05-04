f1cfc63b69b7e4e062831545f21da5dc3d867ab2
Vicente <vicente.buendia@icloud.com>
2022-05-04 17:59:08 +0000

    database

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarMake
+
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
 
 # Register models here
+admin.site.register(CarMake)
+admin.site.register(CarModel)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.timezone import now
 
+from django.conf import settings
 
 # Create your models here.
 
@@ -9,6 +10,39 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+     name =  models.CharField(max_length=200, default="Car Make")
+     description =  models.CharField(max_length=200, default="Description")
+     
+     def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    name = models.CharField(null=False, max_length=30, default='Car Dealer')
+    description = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return "Name: " + self.name + "," + \
+               "Description: " + self.description
+
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview(models.Model):
+    dealer = models.ForeignKey(
+        CarDealer,
+        on_delete=models.CASCADE,
+     )    
+    description = models.CharField(max_length=2000)
+    reviewer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    
+    def __str__(self):
+        return "Reviewer: " + self.reviewer + "," + \
+               "Dealer: " + self.dealer + "," + \
+               "Description: " + self.description
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +53,30 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+     car_makes = models.ManyToManyField(CarMake)
+     dealer_id = models.IntegerField(default=0)
+     name = models.CharField(max_length=200, default="Car Model Name")
+     SEDAN = 'Sedan'
+     SUV = 'SUV'
+     WAGON = 'WAGON'
+     TYPE_CHOICES = [
+        (SEDAN , 'Sedan'),
+        (SUV , 'SUV'),
+        (WAGON , 'WAGON')        
+     ]
+     model_type = models.CharField(
+        null=False,
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default=SEDAN
+     )
+     year = models.DateField(null=True)
+
+     def __str__(self):
+        return "Name: " + self.name + "," + \
+                "Type :" + self.model_type + \
+                "Date: " + self.year.strftime("%Y")
+            
 
 
-# <HINT> Create a plain Python class `CarDealer` to hold dealer data
-
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
